39f3baf169ca2014521cc8f3b2a493f8292d5e38
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-10-17 00:01:13 -0300

    implementa rotas GET e POST para gerenciamento de usuários

--- aulas/02-fundamentos-node-js/src/server.js
+++ aulas/02-fundamentos-node-js/src/server.js
@@ -1,11 +1,21 @@
 import http from 'http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   } else if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Carlos Henrique',
+      email: 'teste.example@example.com'
+    })
+
     return res.end('Criação de usuário')
   } else {
     return res.end('Hello World')
@@ -14,11 +24,18 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000)
 
-// ➜ http POST localhost:3000/users
+// ➜ http GET localhost:3000/users 
 // HTTP/1.1 200 OK
 // Connection: keep-alive
-// Content-Length: 21
-// Date: Thu, 16 Oct 2025 22:27:27 GMT
+// Content-Length: 71
+// Content-type: application/json
+// Date: Fri, 17 Oct 2025 02:55:21 GMT
 // Keep-Alive: timeout=5
 
-// Criação de usuário
+// [
+//     {
+//         "email": "teste.example@example.com",
+//         "id": 1,
+//         "name": "Carlos Henrique"
+//     }
+// ]
