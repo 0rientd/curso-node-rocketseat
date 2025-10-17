24dc5e5bfee0053c78c313480921818e73d66804
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-10-17 12:48:11 -0300

    corrige resposta da criação de usuário e ajusta cabeçalhos de resposta

--- aulas/02-fundamentos-node-js/src/server.js
+++ aulas/02-fundamentos-node-js/src/server.js
@@ -16,14 +16,26 @@ const server = http.createServer((req, res) => {
       email: 'teste.example@example.com'
     })
 
-    return res.end('Criação de usuário')
+    return res.writeHead(201).end()
   } else {
-    return res.end('Hello World')
+    return res.writeHead(404).end()
   }
 })
 
 server.listen(3000)
 
+// Exemplo 1
+//
+// ➜ http POST localhost:3000/users     
+// HTTP/1.1 201 Created
+// Connection: keep-alive
+// Date: Fri, 17 Oct 2025 15:46:04 GMT
+// Keep-Alive: timeout=5
+// Transfer-Encoding: chunked
+//
+
+// Exemplo 2
+//
 // ➜ http GET localhost:3000/users 
 // HTTP/1.1 200 OK
 // Connection: keep-alive
@@ -39,3 +51,4 @@ server.listen(3000)
 //         "name": "Carlos Henrique"
 //     }
 // ]
+//
