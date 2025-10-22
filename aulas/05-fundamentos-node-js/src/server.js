bd1d83d1314873a8fae62f32aaf3dd366f8696d2
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-10-22 17:42:09 -0300

    atualiza lógica de correspondência de rotas e implementa regex para parâmetros dinâmicos

--- aulas/05-fundamentos-node-js/src/server.js
+++ aulas/05-fundamentos-node-js/src/server.js
@@ -9,10 +9,23 @@ const server = http.createServer(async (req, res) => {
 
   // Estou retornando o objeto que bate com o método e a URL da requisição
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    // console.log(routeParams)
+    // [
+    //   '/users/6b0fcafb-5d13-427c-b644-edcc1ad11dbf',
+    //   '6b0fcafb-5d13-427c-b644-edcc1ad11dbf',
+    //   index: 0,
+    //   input: '/users/6b0fcafb-5d13-427c-b644-edcc1ad11dbf',
+    //   groups: [Object: null prototype] {
+    //     id: '6b0fcafb-5d13-427c-b644-edcc1ad11dbf'
+    //   }
+    // ]
+
     return route.handler(req, res)
   }
 
